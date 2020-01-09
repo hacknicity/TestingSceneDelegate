aa9c72fce4271af0a86289ee50e60d6c46d60753
Geoff Hackworth <geoff@hacknicity.com>
2020-01-09 08:45:39 +0000

    Initial Commit. Xcode 11.3's Single View App template, including unit tests. Removed my Team name from the Signing & Capabilities section for both targets.

---
+++ TestingSceneDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingSceneDelegate
+//
+//  Created by Geoff Hackworth on 09/01/2020.
+//  Copyright © 2020 Hacknicity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
