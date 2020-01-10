5951f7ab950de1557d3538da56b9c29f9c38e5c2
Geoff Hackworth <geoff@hacknicity.com>
2020-01-10 11:35:14 +0000

    Added code to TestingAppDelegate to destroy any cached scene configurations and ensure TestingSceneDelegate is used when running unit tests.

--- TestingSceneDelegateTests/TestingAppDelegate.swift
+++ TestingSceneDelegateTests/TestingAppDelegate.swift
@@ -13,6 +13,12 @@ final class TestingAppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // Remove any cached scene configurations to ensure that TestingAppDelegate.application(_:configurationForConnecting:options:) is called and TestingSceneDelegate will be used when running unit tests. NOTE: THIS IS PRIVATE API AND MAY BREAK IN THE FUTURE!
+        for sceneSession in application.openSessions {
+            application.perform(Selector(("_removeSessionFromSessionSet:")), with: sceneSession)
+        }
+
         return true
     }
 
